ca19e6e9553f08cc51007dad197726412d1e1f83
onikan27 <ude0721juji@icloud.com>
2021-06-06 17:44:43 +0900

    feat: test microCMS

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,45 @@
 import { NextPage } from 'next'
 import Head from 'next/head'
 
-const Home: NextPage = () => {
+type Article = {
+  id: string
+  title: string
+  body: string
+}
+
+type props = {
+  articles: Article[]
+}
+
+const Home: React.FC<props> = ({ articles }) => {
   return (
     <>
       <Head>
-        <title>Talktape</title>
+        <title>Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div />
+      {articles.map((article) => (
+        <div key={article.id}>
+          <h1>{article.title}</h1>
+          <p>{article.id}</p>
+        </div>
+      ))}
     </>
   )
+}
+
+export const getStaticProps = async () => {
+  const key = {
+    headers: { 'X-API-KEY': process.env.API_KEY || '' },
+  }
+
+  const res = await fetch(`${process.env.API_ENDPOINT}/articles`, key)
+  const data = await res.json()
+  return {
+    props: {
+      articles: data.contents,
+    },
+  }
 }
 
 export default Home

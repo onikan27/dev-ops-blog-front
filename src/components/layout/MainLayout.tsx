9c69ea20df6505b7cabb6b208a6f43ec402d9c9d
onikan27 <ude0721juji@icloud.com>
2021-06-10 08:54:46 +0900

    fix: MainLayout

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,9 +1,15 @@
-import { Box } from '@chakra-ui/layout'
+import { Box, Flex } from '@chakra-ui/layout'
+import { RightSideBar } from 'src/components/molecules/RightSideBar'
 
 export const MainLayout: React.FC = ({ children }) => {
   return (
     <Box minH="100vh" bgColor="bg.gray">
-      {children}
+      <Flex maxW="1100px" mx="auto" flexDirection="column">
+        <Flex justifyContent="space-around" mt="32px" mx="auto" w="100%">
+          {children}
+          <RightSideBar />
+        </Flex>
+      </Flex>
     </Box>
   )
 }

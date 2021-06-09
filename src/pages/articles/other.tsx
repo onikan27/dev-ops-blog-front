102d0f4e870f1e7f25363c388c2947656b903007
onikan27 <ude0721juji@icloud.com>
2021-06-10 08:52:46 +0900

    feat: Add pages

---
+++ src/pages/articles/other.tsx
@@ -0,0 +1,30 @@
+import { Box, Text, Flex } from '@chakra-ui/react'
+import Head from 'next/head'
+import { DefaultLayout } from 'src/components/layout/DefaultLayout'
+import { MainLayout } from 'src/components/layout/MainLayout'
+import { Articles } from 'src/components/page/common/articles/Articles'
+
+const Home: React.FC = ({ children }) => {
+  return (
+    <>
+      <Head>
+        <title>Blog</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <DefaultLayout>
+        <MainLayout>
+          <Flex flexDirection="column">
+            <Box mb="32px">
+              <Text fontSize="32px" fontWeight="bold">
+                Other
+              </Text>
+            </Box>
+            <Articles />
+          </Flex>
+        </MainLayout>
+      </DefaultLayout>
+    </>
+  )
+}
+
+export default Home

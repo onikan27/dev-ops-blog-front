c96b033bf88e83cf19b10190feb8f2c4802c4991
onikan27 <ude0721juji@icloud.com>
2021-06-10 07:31:22 +0900

    fix: chore

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import { Flex, Box } from '@chakra-ui/layout'
 import { DefaultLayout } from 'src/components/layout/DefaultLayout'
 import { MainLayout } from 'src/components/layout/MainLayout'
 import { RightSideBar } from 'src/components/molecules/RightSideBar'
+import { Articles } from 'src/components/page/common/articles/Articles'
 
 const Home: React.FC = ({ children }) => {
   return (
@@ -13,9 +14,9 @@ const Home: React.FC = ({ children }) => {
       </Head>
       <DefaultLayout>
         <MainLayout>
-          <Flex maxW="1000px" mx="auto" flexDirection="column">
-            <Flex justifyContent="space-between" mt="32px">
-              <Box>メインコンテンツ</Box>
+          <Flex maxW="1100px" mx="auto" flexDirection="column">
+            <Flex justifyContent="space-around" mt="32px" mx="auto" w="100%">
+              <Articles />
               {/* {children} */}
               <RightSideBar />
             </Flex>

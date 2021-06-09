bf22931c4eb8f87a28d9492a3f6751798218dbde
onikan27 <ude0721juji@icloud.com>
2021-06-10 08:56:25 +0900

    feat: fix ui

--- src/components/page/common/articles/Article.tsx
+++ src/components/page/common/articles/Article.tsx
@@ -1,5 +1,6 @@
 import { Box, Image, Flex, Text } from '@chakra-ui/react'
 import Tag from 'src/components/atoms/Tag'
+import Link from 'next/link'
 
 const tags = [
   { name: 'Rails' },
@@ -10,38 +11,49 @@ const tags = [
 
 export const Article: React.FC = () => {
   return (
-    <Flex bg="white.white" w="100%" maxW="750px" h="200px" borderRadius="8px">
-      <Box w="35%">
-        <Image
-          src="Image/test.png"
-          borderRadius="8px"
-          w="100%"
-          h="100%"
-          objectFit="cover"
-        />
-      </Box>
-      <Box p="16px" w="65%" position="relative">
-        <Box mb="16px">
-          <Text fontSize="24px" fontWeight="bold">
-            タイトル
-          </Text>
+    <Link href="/">
+      <Flex
+        bg="white.white"
+        w="100%"
+        maxW="750px"
+        h="200px"
+        cursor="pointer"
+        borderRadius="8px"
+        boxShadow="0 1px 8px 0 rgb(0 0 0 / 15%)"
+        _hover={{ boxShadow: '0 1px 8px 0 rgb(0 0 0 / 30%)' }}
+      >
+        <Box w="35%">
+          <Image
+            src="/Image/test.png"
+            borderRadius="8px"
+            w="100%"
+            h="100%"
+            objectFit="cover"
+          />
         </Box>
-        <Box mb="16px">
-          <Text color="text.gray" fontSize="14px">
-            ディスクリプションディスクリプションディスクリプションディスクリプションディスクリプションディスクリプションディスクリプションディスクリプションディスクリプション
-          </Text>
+        <Box p="16px" w="65%" position="relative">
+          <Box mb="16px">
+            <Text fontSize="24px" fontWeight="bold">
+              タイトル
+            </Text>
+          </Box>
+          <Box mb="16px">
+            <Text color="text.gray" fontSize="14px">
+              ディスクリプションディスクリプションディスクリプションディスクリプションディスクリプションディスクリプションディスクリプションディスクリプションディスクリプション
+            </Text>
+          </Box>
+          <Flex flexWrap="wrap">
+            {tags.map((tag) => (
+              <Tag tag={tag} key={tag.name} />
+            ))}
+          </Flex>
+          <Box position="absolute" right="8px" bottom="8px">
+            <Text fontSize="12px" color="text.gray">
+              2021年5月21日
+            </Text>
+          </Box>
         </Box>
-        <Flex flexWrap="wrap">
-          {tags.map((tag) => (
-            <Tag tag={tag} key={tag.name} />
-          ))}
-        </Flex>
-        <Box position="absolute" right="8px" bottom="8px">
-          <Text fontSize="12px" color="text.gray">
-            2021年5月21日
-          </Text>
-        </Box>
-      </Box>
-    </Flex>
+      </Flex>
+    </Link>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,7 @@
+import { Box, Text, Flex } from '@chakra-ui/react'
 import Head from 'next/head'
-import { Flex, Box } from '@chakra-ui/layout'
 import { DefaultLayout } from 'src/components/layout/DefaultLayout'
 import { MainLayout } from 'src/components/layout/MainLayout'
-import { RightSideBar } from 'src/components/molecules/RightSideBar'
 import { Articles } from 'src/components/page/common/articles/Articles'
 
 const Home: React.FC = ({ children }) => {
@@ -14,12 +13,13 @@ const Home: React.FC = ({ children }) => {
       </Head>
       <DefaultLayout>
         <MainLayout>
-          <Flex maxW="1100px" mx="auto" flexDirection="column">
-            <Flex justifyContent="space-around" mt="32px" mx="auto" w="100%">
-              <Articles />
-              {/* {children} */}
-              <RightSideBar />
-            </Flex>
+          <Flex flexDirection="column">
+            <Box mb="32px">
+              <Text fontSize="32px" fontWeight="bold">
+                New
+              </Text>
+            </Box>
+            <Articles />
           </Flex>
         </MainLayout>
       </DefaultLayout>

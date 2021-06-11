0782cea89ee08533db208664176d0fe0376883aa
onikan27 <ude0721juji@icloud.com>
2021-06-11 19:45:23 +0900

    feat: Add index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,20 @@
+import { NextPage, GetStaticProps } from 'next'
 import { Box, Text, Flex } from '@chakra-ui/react'
 import Head from 'next/head'
 import { DefaultLayout } from 'src/components/layout/DefaultLayout'
 import { MainLayout } from 'src/components/layout/MainLayout'
 import { Articles } from 'src/components/page/common/articles/Articles'
+import { ApiKey } from 'utils/api-key'
+import { ArticleType } from 'types'
+import { RightSideBar } from 'src/components/molecules/RightSideBar'
+import { TagType } from 'types'
 
-const Home: React.FC = ({ children }) => {
+type props = {
+  articles: ArticleType[]
+  tags: TagType[]
+}
+
+const Home: NextPage<props> = ({ articles, tags }) => {
   return (
     <>
       <Head>
@@ -19,12 +29,32 @@ const Home: React.FC = ({ children }) => {
                 New
               </Text>
             </Box>
-            <Articles />
+            <Articles articles={articles} />
           </Flex>
+          <RightSideBar tags={tags} />
         </MainLayout>
       </DefaultLayout>
     </>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const key = ApiKey()
+  const resArticles = await fetch(
+    `${process.env.NEXT_PUBLIC_ENDPOINT}/articles`,
+    key,
+  )
+  const articles = await resArticles.json()
+
+  const resTags = await fetch(`${process.env.NEXT_PUBLIC_ENDPOINT}/tags`, key)
+  const tags = await resTags.json()
+
+  return {
+    props: {
+      articles: articles.contents,
+      tags: tags.contents,
+    },
+  }
 }
 
 export default Home

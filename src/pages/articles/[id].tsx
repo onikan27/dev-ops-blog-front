fdc71854e11a9aa68d3d82469aa92b807fc8a649
onikan27 <ude0721juji@icloud.com>
2021-07-24 17:47:15 +0900

    gix

--- src/pages/articles/[id].tsx
+++ src/pages/articles/[id].tsx
@@ -86,7 +86,7 @@ export const getStaticPaths: GetStaticPaths = async () => {
     (path: { id: string }) => `/articles/${path.id}`,
   )
 
-  return { paths, fallback: false }
+  return { paths, fallback: 'blocking' }
 }
 
 export default App
